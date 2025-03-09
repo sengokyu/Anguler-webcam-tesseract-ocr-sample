a60102457660acab5d144850d074c293396316b9
sengokyu <sengokyu+gh@gmail.com>
2025-03-09 09:43:52 +0900

    Add stop button behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { CameraOcrComponent } from './camera-ocr/camera-ocr.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, CameraOcrComponent],
+  imports: [CameraOcrComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/camera-ocr/camera-ocr.component.ts
+++ src/app/camera-ocr/camera-ocr.component.ts
@@ -3,17 +3,24 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  OnDestroy,
   OnInit,
+  signal,
   ViewChild,
 } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { from, map, Observable, throwError } from 'rxjs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import {
   enumerateVideoInputDevices,
   getUserMedia,
 } from '../utils/user-media-utils';
+
+// 停止ボタンの表示
+type StopType = 'stop' | undefined;
 
 @Component({
   selector: 'camera-ocr',
@@ -21,37 +28,81 @@ import {
     AsyncPipe,
     CommonModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
     MatSelectModule,
+    MatToolbarModule,
   ],
   templateUrl: './camera-ocr.component.html',
   styleUrl: './camera-ocr.component.scss',
 })
-export class CameraOcrComponent implements OnInit, AfterViewInit {
+export class CameraOcrComponent implements OnInit, AfterViewInit, OnDestroy {
   devices$: Observable<MediaDeviceInfo[]> | undefined;
   readonly deviceFc = new FormControl<MediaDeviceInfo | null>(null);
+  readonly hideCanvas = signal<boolean>(true);
+  readonly playStop$ = new BehaviorSubject<StopType>(undefined);
 
   @ViewChild('video', { static: true })
   private videoElement!: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas', { static: true })
+  private canvasElement!: ElementRef<HTMLCanvasElement>;
 
   constructor() {}
 
   ngOnInit(): void {
     this.devices$ = enumerateVideoInputDevices();
     this.deviceFc.valueChanges.subscribe((device) => {
-      this.onDeviceFcChange(device);
+      this.onChangeDeviceFc(device);
     });
   }
 
   ngAfterViewInit(): void {}
 
-  private onDeviceFcChange(value: MediaDeviceInfo | null): void {
+  ngOnDestroy(): void {
+    // ビデオを停止
+    this.stopTracks();
+  }
+
+  onClick(): void {
+    const canvas = this.canvasElement.nativeElement;
+    const context = canvas.getContext('2d');
+
+    context?.drawImage(this.video, 0, 0, canvas.width, canvas.height);
+
+    this.hideCanvas.set(false);
+  }
+
+  onClickClose(): void {
+    this.hideCanvas.set(true);
+  }
+
+  onClickPlayStop(): void {
+    if (!this.stream) {
+      return;
+    }
+
+    this.stopTracks();
+  }
+
+  private get video(): HTMLVideoElement {
+    return this.videoElement.nativeElement;
+  }
+
+  private get stream(): MediaStream | null | undefined {
+    return this.video.srcObject as MediaStream;
+  }
+
+  private stopTracks(): void {
+    this.deviceFc.setValue(null, { emitEvent: false });
+    this.stream?.getVideoTracks().forEach((track) => track.stop());
+  }
+
+  private onChangeDeviceFc(value: MediaDeviceInfo | null): void {
     if (value) {
       getUserMedia(value).subscribe((stream) => {
-        const video = this.videoElement.nativeElement;
-
-        video.srcObject = stream;
-        video.play();
+        this.video.srcObject = stream;
+        this.video.play();
+        this.playStop$.next('stop');
       });
     }
   }
